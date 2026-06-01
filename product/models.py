2f0bb65c64b009d1132ee3bbe35564f5f918fcf8
profluizdias <luizdias01@prof.educacao.sp.gov.br>
2026-06-01 12:09:06 -0300

    add routes and views

--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    code = models.TextField(max_length=20, blank=False, null=False)
+    name = models.TextField(max_length=250, blank=False, null=False)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    picture = models.ImageField(upload_to='products/', blank=True, null=True)
+    qtt = models.IntegerField(blank=False, null=False)
+    unity = models.TextField(max_length=3, blank=False, null=False)
+    price = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False)
+
+    def __str__(self):
+        return f'{self.code} - {self.name}'
