483128ebed8b5393d062684fb1c609e22afc336c
LuizCD <luizcdprof@gmail.com>
2026-06-15 11:37:41 -0300

    endpoint create_user_profile

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from userprofile.views import userprofile_view
+from userprofile.views import userprofile_view, userprofile_register
 
 urlpatterns = [
     path('', userprofile_view, name='userprofile_view'),
+    path('register/', userprofile_register, name='userprofile_register_view')
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,5 +1,9 @@
+import json
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 # Create your views here.
 def home_view(request):
@@ -7,3 +11,34 @@ def home_view(request):
 
 def userprofile_view(request):
     return HttpResponse("<h1>User Profile</h1>")
+
+@csrf_exempt
+def userprofile_register(request):
+    if request.method == 'POST':
+        # 1. Transforma o JSON do Bruno em um dicionário python
+        data = json.loads(request.body)
+
+        # 2. Cria o User padrão do Django (criptografando a senha)
+        new_user = User.objects.create_user(
+            username = data['username'],
+            password = data['password'],
+            email = data['email']
+        )
+
+        # 3. Cria o UserProfile vinculando ao User criado acima
+        new_user_profile = UserProfile.objects.create(
+            user = new_user,
+            birthdate = data['birthdate']
+        )
+
+        return JsonResponse(
+            {
+                'status': 'Sucesso',
+                'message': 'Usuário criado!',
+            }, status=201
+        )
+    return JsonResponse(
+        {
+            'erro': 'Método não permitido'
+        }, status=405
+    )
