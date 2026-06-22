ef6a4835722c66e3cae99c6157d6c299b9e12abe
LuizCD Prof <luizcdprof@gmail.com>
2026-06-21 21:22:27 -0300

    adds bruno api collection and userprofile views

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from userprofile.views import userprofile_view, userprofile_register
+from userprofile.views import userprofile_login, userprofile_view, userprofile_list, userprofile_register
 
 urlpatterns = [
-    path('', userprofile_view, name='userprofile_view'),
+    path('login/', userprofile_login, name='userprofile_login'),
+    path('view/', userprofile_view, name='userprofile_view'),
+    path('list/', userprofile_list, name='userprofile_list'),
     path('register/', userprofile_register, name='userprofile_register_view')
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,17 +1,87 @@
 import json
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from .models import UserProfile
+
+# superuser - admin - t3st3s3c
 
 # Create your views here.
 def home_view(request):
     return render(request, 'home.html')
 
-def userprofile_view(request):
-    return HttpResponse("<h1>User Profile</h1>")
+@csrf_exempt
+def userprofile_login(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
 
+        user = User.objects.filter(username=username).first()
+        if user and user.check_password(password):
+            if user is not None:
+                login(request, user)  # <--- ESSENCIAL: Isso cria a sessão e prepara o cookie
+            return JsonResponse({'success': True, 'message': 'Login successful'})
+        else:
+            return JsonResponse({'success': False, 'message': 'Invalid credentials'}, status=401)
+
+    return JsonResponse({'error': 'Method not allowed'}, status=405)
+
+@login_required
+@csrf_exempt
+def userprofile_view(request):
+    if request.method == 'POST':
+        usuario = request.user
+        # Resposta para requisição AJAX (JSON)
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            profile = UserProfile.objects.filter(user=usuario).first()
+            usuario_data = {
+                'id': usuario.id,
+                'username': usuario.username,
+                'email': usuario.email,
+                'birthdate': profile.birthdate.isoformat() if profile and profile.birthdate else None,
+            }
+            return JsonResponse({'success': True, 'usuario': usuario_data})
+
+    return JsonResponse(
+        {
+            'erro': 'Método não permitido'
+        }, status=405
+    )
+
+@login_required
+@csrf_exempt
+def userprofile_list(request):
+    if request.method == 'POST':
+        usuario = request.user
+        if usuario.is_staff:  # Apenas usuários com permissão de staff podem acessar a lista completa
+            usuarios = UserProfile.objects.all()
+            usuarios_data = []
+
+            # Resposta para requisição AJAX (JSON)
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                # Converte o objeto de usuário em um dicionário para serializar
+                for usuario in usuarios:
+                    usuarios_data.append({
+                        'id': usuario.id,
+                        'username': usuario.user.username,
+                        'email': usuario.user.email,
+                        'birthdate': usuario.birthdate
+                    })
+                return JsonResponse({'success': True, 'usuarios': usuarios_data})
+        else:
+            return JsonResponse({'success': False, 'message': 'Unauthorized'}, status=401)
+
+    return JsonResponse(
+        {
+            'erro': 'Método não permitido'
+        }, status=405
+    )
+
+@login_required
 @csrf_exempt
 def userprofile_register(request):
     if request.method == 'POST':
